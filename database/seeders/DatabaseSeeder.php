9879a021b8877f37f4cc75aa55e43edab2899c2e
Shamseer-tt <shamseer.primalcodes@gmail.com>
2023-02-16 07:38:57 +0530

    user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use Illuminate\Support\Facades\DB;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert(
+            ['name'=>'superadmin',
+            'email'=>'admin@admin.gmail.com',
+            'password'=>bcrypt('password'),
+            'created_at'=>date('Y-m-d H:i:s'),
+            'updated_at'=>date('Y-m-d H:i:s')]
+        );
     }
 }
